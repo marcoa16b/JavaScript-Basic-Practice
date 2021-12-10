49bd27eaae07a4812e8ddc8b643b143686e31b25
marcoa16b <62965695+marcoa16b@users.noreply.github.com>
2021-12-09 20:15:39 -0600

    Curso terminado

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # JavaScript-Basic-Practice
+
+...
+
+## Taller #1: figuras geometricas
+
+- 1 definir las formulas
+- 2 implementar las formulas en javascript
+- 3 crear funciones
+- 4 integrar JS con HTML
---
+++ Reto 2/descCupones.js
@@ -0,0 +1,51 @@
+const coupons = [
+    "JD_es_Batman",
+    "pero_no_le_digas_a_nadie",
+    "es_un_secreto",
+];
+
+// const precioOriginal = 120;
+// const descuento = 18;
+
+
+function calcularPrecioConDescuento(precio, descuento) {
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+
+    return precioConDescuento;
+}
+
+function onClickButtonPriceDiscount() {
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+    
+    const inputCoupon = document.getElementById("InputCoupon");
+    const couponValue = inputCoupon.value;
+
+    let descuento;
+
+    switch(couponValue) {
+        case coupons[0]:
+            descuento = 15;
+        break;
+        case coupons[1]:
+            descuento = 30;
+        break;
+        case coupons[2]:
+            descuento = 25;
+        break;
+    }
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
+
+    const resultP = document.getElementById("ResultP");
+    resultP.innerText = "El precio con descueto son: $" + precioConDescuento;
+}
+
+
+// console.log({
+//   precioOriginal,
+//   descuento,
+//   porcentajePrecioConDescuento,
+//   precioConDescuento,
+// });
